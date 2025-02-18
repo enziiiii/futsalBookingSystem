3fa5d1202a5e93c8953b0457e7ea6892b359e7bb
enziiiii <enzii.grg00@gmail.com>
2025-02-18 19:04:24 +0545

    moved allRouter to router folder and deleted merge-router folder

---
+++ backend/server/router/allRouter.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const authRouter = require("./auth-router");
+const userRouter = require("./user-router");
+
+// Use sub-routers
+router.use("/auth", authRouter);
+router.use("/user", userRouter);
+
+module.exports = router;
