8bbd5cca74470e6b45f7c9fdaf6922d66d46f8f3
enziiiii <enzii.grg00@gmail.com>
2025-02-23 01:40:46 +0545

    updated createUserTable function

--- backend/server/db/createUserTable.js
+++ backend/server/db/createUserTable.js
@@ -5,22 +5,32 @@ const createUserTable = async () => {
     const queryText = `
     CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
-    full_name VARCHAR(100),
     username VARCHAR UNIQUE NOT NULL,
+    full_name VARCHAR(100),
     email VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     phone_number VARCHAR(20),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
+
     `;
+    // const client = await pool.connect();
     try {
+        // await client.query("BEGIN"); 
+
         await pool.query(queryText);
+
+        // await client.query("COMMIT");
         console.log("User table created if not exists");
     } catch (error) {
+        // await client.query("ROLLBACK");
         console.log("Error creating users table : ", error);
         throw error;
     } 
+    // finally {
+    //     client.release();
+    // }
 };
 
 module.exports = createUserTable;
