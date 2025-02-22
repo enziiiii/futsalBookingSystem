3dab08d62276b4a9ceb5cdd89ce3947862b0a95d
enziiiii <enzii.grg00@gmail.com>
2025-02-23 01:43:57 +0545

    udpated the user-router file from router

--- backend/server/router/user-router.js
+++ backend/server/router/user-router.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 router.post("/", userController.createUserController);
 router.get("/", userController.getAllUsersController);
-router.get("/:id", userController.getUserByIdController);
-router.put("/:id", userController.updateUserController);
-router.delete("/:id", userController.deleteUserController);
+router.get("/:userId", userController.getUserByIdController);
+router.put("/:userId", userController.updateUserController);
+router.delete("/:userId", userController.deleteUserController);
 
 module.exports = router;
