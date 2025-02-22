624d1da42d4e57ac7128156deb6cdde37565b97d
enziiiii <enzii.grg00@gmail.com>
2025-02-22 14:20:43 +0545

    added allTable.js in db for all the tables in one place

---
+++ backend/server/db/AllTable.js
@@ -0,0 +1,24 @@
+const { pool } = require('../config/db');
+const createRoleTable = require("./createRoleTable");
+const createUserTable = require("./createUserTable");
+
+const initializeSchemaTable = async () => {
+    const client = await pool.connect();
+    try {
+        await client.query("BEGIN"); 
+
+        // Reuse modular functions
+        await createUserTable(client);
+        await createRoleTable(client);
+
+        await client.query("COMMIT");
+    } catch (error) {
+        await client.query("ROLLBACK");
+        console.error('Schema initialization failed:', error);
+        throw error;
+    } finally {
+        client.release();
+    }
+};
+
+module.exports = initializeSchemaTable;
