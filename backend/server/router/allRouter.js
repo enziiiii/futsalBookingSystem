f5e7289bd836eebedc9c6fa4c4fdb1af0b64f3c6
enziiiii <enzii.grg00@gmail.com>
2025-02-25 17:56:47 +0545

    add bcrypt in the modules

--- backend/server/router/allRouter.js
+++ backend/server/router/allRouter.js
@@ -1,8 +1,10 @@
+// this file is for all router in one place
+// we can directly write in server.js but it would make server.js more traffic code in one place
 const express = require("express");
 const router = express.Router();
 
-const authRouter = require("./auth-router");
-const userRouter = require("./user-router");
+const authRouter = require("../auth-router.js");
+const userRouter = require("../user-router.js");
 
 // Use sub-routers
 router.use("/auth", authRouter);
--- backend/server/services/user-service.js
+++ backend/server/services/user-service.js
@@ -1,39 +1,39 @@
-const userModel = require('../models/user-model');
-const roleModel = require('../models/role-model');
-const bcrypt = require('bcrypt');
+// const userModel = require('../models/user-model');
+// const roleModel = require('../models/role-model');
+// const bcrypt = require('bcrypt');
 
-const SALT_ROUNDS = 10;
+// const SALT_ROUNDS = 10;
 
-const registerUser = async (userData, roles = ['customer']) => {
-  // Hash password
-  const hashedPassword = await bcrypt.hash(userData.password, SALT_ROUNDS);
+// const registerUser = async (userData, roles = ['customer']) => {
+//   // Hash password
+//   const hashedPassword = await bcrypt.hash(userData.password, SALT_ROUNDS);
   
-  // Create user
-  const { rows: [user] } = await userModel.createUser(
-    userData.username,
-    userData.email,
-    hashedPassword,
-    userData.fullName,
-    userData.phone
-  );
+//   // Create user
+//   const { rows: [user] } = await userModel.createUser(
+//     userData.username,
+//     userData.email,
+//     hashedPassword,
+//     userData.fullName,
+//     userData.phone
+//   );
 
-  // Assign roles
-  for (const roleName of roles) {
-    const role = await roleModel.getRoleByName(roleName);
-    if (role) {
-      await userModel.assignUserRole(user.user_id, role.role_id);
-    }
-  }
+//   // Assign roles
+//   for (const roleName of roles) {
+//     const role = await roleModel.getRoleByName(roleName);
+//     if (role) {
+//       await userModel.assignUserRole(user.user_id, role.role_id);
+//     }
+//   }
 
-  return getUserByEmailWithRoles(user.email);
-};
+//   return getUserByEmailWithRoles(user.email);
+// };
 
-const getUserByEmailWithRoles = async (email) => {
-  const { rows: [user] } = await userModel.getUserByEmailWithRoles(email);
-  return user;
-};
+// const getUserByEmailWithRoles = async (email) => {
+//   const { rows: [user] } = await userModel.getUserByEmailWithRoles(email);
+//   return user;
+// };
 
-module.exports = {
-  registerUser,
-  getUserByEmailWithRoles
-};
+// module.exports = {
+//   registerUser,
+//   getUserByEmailWithRoles
+// };
