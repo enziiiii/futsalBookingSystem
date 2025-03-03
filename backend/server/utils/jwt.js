c7ca1b4123b3d23450d4af8842228c79b714f2df
enziiiii <enzii.grg00@gmail.com>
2025-03-03 15:58:27 +0545

    updated jwt.js

--- backend/server/utils/jwt.js
+++ backend/server/utils/jwt.js
@@ -1,9 +1,23 @@
 const jwt = require("jsonwebtoken");
 
+/* by using payload style
 const generateToken = (payload) => {
     return jwt.sign(payload, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_IN,
     });
+};
+*/
+
+const generateToken = (user) => {
+    return jwt.sign(
+        {
+            userId: user.user_id, // From users table
+            email: user.email,
+            roles: user.roles  // Array from user_roles join
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
 };
 
 const verifyToken = (token) => {
