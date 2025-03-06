53b715cb5bbe27bdcf9a98751c28adaa9f4a1311
enziiiii <enzii.grg00@gmail.com>
2025-03-07 00:09:20 +0545

    updated and added a new function on tokenCleanup.js

--- backend/server/utils/tokenCleanup.js
+++ backend/server/utils/tokenCleanup.js
@@ -1,9 +1,24 @@
+const cron = require('node-cron');
 const { allModels } = require("../models")
 
-cron.schedule("0 0 * * * ", async () => { // runs daily at midnight
+// cron.schedule("0 0 * * *", async () => { // runs daily at midnight
+//     try {
+//         await allModels.tokenBlacklistModel.cleanExpiredTokens();
+//         // await allModels.tokenBlacklist.cleanExpiredTokens();
+//     } catch (error) {
+//         console.error("Token cleanup failed:", error);
+//     }
+// });
+
+const cleanUpExpiredTokens = async () => {
     try {
+        console.log("Running token cleanup job");
         await allModels.tokenBlacklist.cleanExpiredTokens();
+        console.log("Expired tokens removed.");
     } catch (error) {
-        console.error("Token cleanup failed:", error);
-    }
-});
+        console.error("Error in token cleanup job:", error);    }
+};
+
+cron.schedule("0 0 * * *", cleanUpExpiredTokens);
+
+module.exports = cleanUpExpiredTokens;
