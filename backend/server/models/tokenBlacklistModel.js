21d20780714dacc6f658b3dd05275cd26d3d201b
enziiiii <enzii.grg00@gmail.com>
2025-03-10 01:34:34 +0545

    added tokenBlacklist for keeping blaclist token

---
+++ backend/server/models/tokenBlacklistModel.js
@@ -0,0 +1,34 @@
+class TokenBlacklist {
+
+    async tokenExists(token) {
+        const query = `
+            SELECT EXISTS(
+                SELECT 1 FROM token_blacklist
+                WHERE token = $1 LIMIT 1
+            ) AS is_revoked;
+        `;
+        const result = await db.query(query, [token]);
+        return result.rows[0].is_revoked;
+    }
+
+    async addToken(token, expiresAt) {
+        // add token to blacklist
+        const query = `
+            INSERT INTO token_blacklist (token, expires_at)
+            VALUES ($1, $2)
+            ON CONFLICT (token) DO NOTHING;
+        `;
+
+        await db.query(query, [token, expiresAt]);
+    }
+
+    async cleanExpiredTokens() {
+        const query = `
+            'DELETE FROM token_blacklist
+            WHERE expires_at < NOW()
+        `;
+        await db.query(query);
+    }
+}
+
+module.exports = new TokenBlacklist();
