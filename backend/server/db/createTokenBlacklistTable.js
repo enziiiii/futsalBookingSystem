9075de4ce65dff3f90341bff11c53419d6645064
enziiiii <enzii.grg00@gmail.com>
2025-03-21 12:03:13 +0545

    updated createTokenBlacklistTable.js

--- backend/server/db/createTokenBlacklistTable.js
+++ backend/server/db/createTokenBlacklistTable.js
@@ -5,14 +5,14 @@ const createTokenBlacklistTable = async () => {
         CREATE TABLE IF NOT EXISTS token_blacklist (
         id SERIAL PRIMARY KEY,
         token TEXT NOT NULL UNIQUE, 
-        expires_at TIMESTAMPZ NOT NULL,  
-        created_at TIMESTAMPZ DEFAULT now()
-)
+        expires_at TIMESTAMPTZ NOT NULL,  
+        created_at TIMESTAMPTZ DEFAULT now()
+    )
     `;
     try {
         await pool.query(queryText);
         console.log("Token Blacklist table created if not exists");
-    } catch {
+    } catch (error) {
         console.log("Error creating Token Blacklist table : ", error);
         throw error;
     }
