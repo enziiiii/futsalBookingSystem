45bdb1077986bcdd66a6cc4eebe5a321adcfb02f
enziiiii <enzii.grg00@gmail.com>
2025-03-21 12:05:57 +0545

    udpated user-router.js

--- backend/server/router/user-router.js
+++ backend/server/router/user-router.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const userController = require("../controllers/user-controller");
 
+const { protect, authorize, renewAccessToken } = require("../middlewares/authMiddleware");
+const { validateUser } = require("../middlewares/inputValidator");
+
 const router = express.Router();
 
-router.post("/", userController.createUserController);
+router.post("/", validateUser('registerSchema'), userController.createUserController);
 router.get("/", userController.getAllUsersController);
 router.get("/:userId", userController.getUserByIdController);
 router.put("/:userId", userController.updateUserController);
 router.delete("/:userId", userController.deleteUserController);
+
+// router.get("/profile", renewAccessToken, protect, userController.getProfile); // need to make a 
+
+
+// admin or staffs
+// router.post("/courts", authorize(['admin', 'staff'], userController.courtController));
 
 module.exports = router;
