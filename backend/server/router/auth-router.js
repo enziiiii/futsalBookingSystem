4b98ef6340d2eff8bcb1dc06266a3ad5478d1c35
enziiiii <enzii.grg00@gmail.com>
2025-03-21 12:05:28 +0545

    updated auth-router.js

--- backend/server/router/auth-router.js
+++ backend/server/router/auth-router.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const rateLimit = require('express-rate-limit');
 const router = express.Router();
 
 const authControllers = require("../controllers/auth-controller");
+const { validateUser } = require("../middlewares/inputValidator");
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 min
+    max: 6, // login/regiter attempts
+    message: "Too many attempts, please try again later"
+});
+
 
 router.route("/").get(authControllers.home);
-router.route("/login").post(authControllers.login);
+router.route("/login").post(limiter, validateUser('loginSchema'), authControllers.login);
 router.route("/logout").post(authControllers.logout);
-router.route("/register").post(authControllers.register);
+router.route("/register").post(validateUser('registerSchema'),authControllers.register);
 
 
 module.exports = router;
