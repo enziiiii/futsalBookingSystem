7ab359cf63a49940b3391543aefd8facca77b2f7
enziiiii <enzii.grg00@gmail.com>
2025-03-23 04:14:30 +0545

    updated admin-router.js

--- backend/server/router/admin-router.js
+++ backend/server/router/admin-router.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const { protect, authorize } = require("../middlewares/authMiddleware");
 const adminController = require("../controllers/admin-controller");
+const { getAllUsers } = require("../models/user-model");
+const { validateCourt } = require("../middlewares/inputValidator");
+const courtController = require("../controllers/court-controller");
 
 const router = express.Router();
 
+// Admin routes
+router.route("/users").get(getAllUsers);
 router.get("/admin-dashboard", protect, authorize(['admin']), adminController.getAdminDashboard);
+
+// Court management routes
+router.post("/courts", authorize(["admin"]), validateCourt('createCourtSchema'), courtController.createCourtController);
+router.get("/courts", authorize(["admin"]), courtController.getAllCourtController);
+router.get("/courts/:courtId", authorize(["admin"]), courtController.getCourtByIdController);
+router.put("/courts/:courtId", authorize(["admin"]), validateCourt('updateScourtSchema'), courtController.updateCourtController);
+router.delete("/courts/:courtId", authorize(["admin"]), courtController.deleteCourtController);
 
 module.exports = router;
