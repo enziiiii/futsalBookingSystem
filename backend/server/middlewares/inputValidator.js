fe920113ab06f2f4c378149f7e31165e853064e8
enziiiii <enzii.grg00@gmail.com>
2025-03-28 09:28:02 +0545

    updated inputValidator.js

--- backend/server/middlewares/inputValidator.js
+++ backend/server/middlewares/inputValidator.js
@@ -1,7 +1,10 @@
 // const Joi = require("joi");
 const { z } = require('zod');
 
+
 const schemas = {
+// ----------- for Authentication and Authorization -------------------------------------//
+
     registerSchema: z.object({
         username: z.string().min(3).max(30),
         fullName: z.string().min(3).max(100),
@@ -17,9 +20,25 @@ const schemas = {
     loginSchema: z.object({
         email: z.string().email(),
         password: z.string().min(8).max(255)
+    }),
+
+    
+    // ----------------- for Court -------------------------------------//
+    
+    createCourtSchema: z.object({
+        court_name: z.string().min(1, "Court name is required"),
+        location: z.string().optional(),
+        hourly_rate : z.number().positive("Hourly rate must be positive"),
+        status: z.enum(["available", "booked", "maintenace", "closed"]).default("available")
+    }),
+
+    updateCourtSchema: z.object({
+        court_name: z.string().min(1).optional(),
+        location: z.string().optional(),
+        hourly_rate: z.number(0).positive().optional(),
+        status: z.enum(["available", "maintenance", "closed"]).optional()
     })
 };
-
 const validateUser = (schema) => (req, res, next) => {
     const parseResult = schemas[schema].safeParse(req.body);
     if(!parseResult.success) { 
@@ -31,39 +50,38 @@ const validateUser = (schema) => (req, res, next) => {
     next();
 };
 
-module.exports = {
-    validateUser
-}
-
-/*
-const registerSchema = Joi.object({
-    username: Joi.string().min(3).max(30).required(),
-    fullName: Joi.string().min(3).max(100).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(8).max(255).pattern(new RegExp('^(?=.*[A-Z])(?=.*[!@#$%^&*])')).required(),
-    phoneNumber: Joi.string().min(10).max(20).required()
-});
-
-const loginSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(8).max(255).required()
-});
-
-
-
-
-const validateUser = (req, res, next) => {
-    const { error } = registerSchema && loginSchema.validate(req.body);
-    if(error) 
+const validateCourt = (schema) => (req, res, next) => {
+    const parseResult = schemas[schema].safeParse(req.body);
+    if (!parseResult.success) {
         return res.status(400).json({
-    status: 400,
-    message: error.details[0].message,
-    });
+            status: 400,
+            message: parseResult.error.errors.map(e => e.message).join(", ")
+        });
+    }
     next();
 };
 
+
 module.exports = {
-    validateUser
+    validateUser,
+    validateCourt
+    
 }
 
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
