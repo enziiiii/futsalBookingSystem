d32243155cc6eae25fc1022c7786041d1afafb4a
enziiiii <enzii.grg00@gmail.com>
2025-03-28 09:29:29 +0545

    updated server.js

--- backend/server/server.js
+++ backend/server/server.js
@@ -2,16 +2,15 @@ require('dotenv').config();
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const router = require("./router/allRouter");
 const { connectDb } = require("./config/db");
 const errorHandling = require('./middlewares/errorHandler');
 
 const cookieParser = require('cookie-parser');
-// const initializeSchemaTable = require('./db/AllTable');
+const initializeSchemaTable = require('./db/AllTable');
 
-
-
-
+app.use(cors());
 
 app.use(cookieParser());
 
@@ -30,7 +29,8 @@ app.use(errorHandling);
 // createUserTable();
 
 
-const PORT = 5000;
+// const PORTS = [5000, 5001];
+const PORT = process.env.PORT || 5000;
 
 async function startServer() {
     try {
@@ -41,10 +41,12 @@ async function startServer() {
         // Create table
         await initializeSchemaTable();
 
+
         // start the server
         app.listen(PORT, () => {
             console.log(`Server running on port ${PORT}`);
         });
+        
     } catch (err) {
         console.error('Server initialization failed:', err);
         process.exit(1);
@@ -52,32 +54,3 @@ async function startServer() {
 }
 
 startServer();
-
-
-// to start the database connection
-/* connectDb().then((isConnected) => {
-    if (!isConnected) {
-        console.error('Cannot start server: Database connection failed');
-        process.exit(0);
-    }
-
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-}); */
-
-// another way to start the connection
-/* const startServer = async () => {
-    const isConnected = await connectDb();
-    if (!isConnected) {
-        console.error('Cannot start server: Database connection failed');
-        process.exit(0);
-    }
-
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-};
-
-startServer();
-*/
