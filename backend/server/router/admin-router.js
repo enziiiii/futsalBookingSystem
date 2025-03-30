da1f3d73bb547940025be042e57f98691c1821a3
enziiiii <enzii.grg00@gmail.com>
2025-03-31 02:14:20 +0545

    updated admin-router.js

--- backend/server/router/admin-router.js
+++ backend/server/router/admin-router.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const { protect, authorize } = require("../middlewares/authMiddleware");
+
 const adminController = require("../controllers/admin-controller");
-const { getAllUsers } = require("../models/user-model");
+// console.log(adminController);
+
+// const { getAllUsers } = require("../models/user-model");
 const { validateCourt } = require("../middlewares/inputValidator");
 const courtController = require("../controllers/court-controller");
-
+const userController = require("../controllers/user-controller");
 const router = express.Router();
 
+
 // Admin routes
-router.route("/users").get(getAllUsers);
+router.route("/admin-UserController").get(userController.getAllUsersController);
 router.get("/admin-dashboard", protect, authorize(['admin']), adminController.getAdminDashboard);
 
+
 // Court management routes
-router.post("/courts", authorize(["admin"]), validateCourt('createCourtSchema'), courtController.createCourtController);
-router.get("/courts", authorize(["admin"]), courtController.getAllCourtController);
-router.get("/courts/:courtId", authorize(["admin"]), courtController.getCourtByIdController);
-router.put("/courts/:courtId", authorize(["admin"]), validateCourt('updateScourtSchema'), courtController.updateCourtController);
-router.delete("/courts/:courtId", authorize(["admin"]), courtController.deleteCourtController);
+router.post("/courts", protect, authorize(["admin"]), validateCourt('createCourtSchema'), courtController.createCourtController);
+router.get("/courts", protect, authorize(["admin"]), courtController.getAllCourtsController);
+// router.get("/courts", courtController.getAllCourtsController);
+
+router.get("/courts/:courtId", protect, authorize(["admin"]), courtController.getCourtByIdController);
+router.put("/courts/:courtId", protect, authorize(["admin"]), validateCourt('updateCourtSchema'), courtController.updateCourtController);
+router.delete("/courts/:courtId", protect, authorize(["admin"]), courtController.deleteCourtController);
 
 module.exports = router;
