55d5f3c516596f31749ffd6d12452f18ec63d3fa
enziiiii <enzii.grg00@gmail.com>
2025-04-02 00:49:36 +0545

    added users: in store.js

--- frontend/client/src/store.js
+++ frontend/client/src/store.js
@@ -2,12 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducers/authSlice";
 import authReducer from './reducers/authSlice';
 import courtReducer from './reducers/courtsSlice';
-
+import userReducer from './reducers/userSlice';
 
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
-        courts: courtReducer
+        courts: courtReducer,
+        users: userReducer
     },
 });
