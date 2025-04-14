1ca1e444fc5bc393297a5e2f45bbcd1160ff5b6e
enziiiii <enzii.grg00@gmail.com>
2025-04-14 19:50:40 +0545

    updated AdminDashboard.jsx

--- frontend/client/src/pages/AdminDashboard/AdminDashboard.jsx
+++ frontend/client/src/pages/AdminDashboard/AdminDashboard.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 
 
 const AdminDashboard = () => {
+  console.log('UserNav rendered')
+  
   return (
     <div>
       <div className="flex h-screen">
@@ -11,12 +13,9 @@ const AdminDashboard = () => {
           <ul>
             {/* <li><Link to="/admin/profile" className="block py-2">Profile</Link></li> */}
             <li><Link to="/admin/courts" className="block py-2">Manage courts</Link></li>
-            <li><Link to="/admin/users" className="block py-2">Manage users</Link></li>
-            {/* <li><Link to="/admin/users"  className="block py-2">Manage users</Link></li> */}
             <li><Link to="/admin/manageCustomers" className="block py-2">Manage Customers</Link></li>
             <li><Link to="/admin/manageStaffs" className="block py-2">Manage Staffs</Link></li>
             <li><Link to="/admin/logOut" className="block py-2">Log Out</Link></li>
-
           </ul>
         </nav>
       </aside>
@@ -25,7 +24,7 @@ const AdminDashboard = () => {
           <p> Welcome to Admin! Below are courts and user manangement</p>
       </main>
     </div>
-    </div>
+  </div>
     
   );
 };
