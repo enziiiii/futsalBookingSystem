d8af4476242ea6bd7136cd900bab80d63dc83ea9
enziiiii <enzii.grg00@gmail.com>
2025-04-14 19:55:10 +0545

    updated store.js

--- frontend/client/src/store.js
+++ frontend/client/src/store.js
@@ -1,14 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import reducer from "./reducers/authSlice";
 import authReducer from './reducers/authSlice';
 import courtReducer from './reducers/courtsSlice';
 import userReducer from './reducers/userSlice';
-
+import bookReducer from './reducers/bookSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         courts: courtReducer,
-        users: userReducer
+        users: userReducer,
+        bookings: bookReducer,
     },
 });
