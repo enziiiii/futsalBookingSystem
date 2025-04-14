5c8e792d22f719b222d4ac69ba199ad36e62400e
enziiiii <enzii.grg00@gmail.com>
2025-04-14 19:45:13 +0545

    updated UserList.jsx

--- frontend/client/src/components/shared/UserList.jsx
+++ frontend/client/src/components/shared/UserList.jsx
@@ -14,23 +14,40 @@ const UserList = ({ users, onEditUser, onDeleteUser }) => {
                 <th className="border p-2">UserName</th>
                 <th className="border p-2">Full Name</th>
                 <th className="border p-2">Email</th>
+                {/* <th className="border p-2">Password</th> */}
+                {/* <th className="border p-2">Role</th> */}
                 <th className="border p-2">Role</th>
                 <th className="border p-2">Actions</th>
             </tr>
         </thead>
         <tbody>
-            {users.map((user) => (
-                <tr key={user.user_id} className="border-t">
-                    <td className="border p-2">{user.username}</td>
-                    <td className="border p-2">{user.full_name}</td>
-                    <td className="border p-2">{user.email}</td>
-                    <td className="border p-2">{user.role}</td>
-                    <td className="border p-2">
-                        <button className="bg-yellow-500 text-white px-2 py-1 rounded mr-2" onClick={() => onEditUser(user)}>Edit</button>
-                        <button className="bg-red-500 text-white px-2 py-1 rounded" onClick={() => handleDelete(user.user_id)}>Delete</button>
+            {/* safety checks */}
+            {Array.isArray(users) && users.length > 0 ? (
+                 users.map((user) => (
+                    <tr key={user.user_id} className="border-t">
+                        <td className="border p-2">{user.username}</td>
+                        <td className="border p-2">{user.full_name}</td>
+                        <td className="border p-2">{user.email}</td>
+                        {/* <td className="border p-2">{user.password_hash}</td> */}
+                        {/* <td className="border p-2">{user.role}</td> */}
+                        <td className="border p-2">
+                            {user.roles 
+                            ? user.roles.join(', ') 
+                            : 'No roles assigned'}
+                        </td>
+                        <td className="border p-2">
+                            <button className="bg-yellow-500 text-white px-2 py-1 rounded mr-2" onClick={() => onEditUser(user)}>Edit</button>
+                            <button className="bg-red-500 text-white px-2 py-1 rounded" onClick={() => handleDelete(user.user_id)}>Delete</button>
+                        </td>
+                    </tr>
+                ))
+            ) : ( 
+                <tr>
+                    <td colSpan="5" className="text-center py-4">
+                        No users found
                     </td>
                 </tr>
-            ))}
+            )}
         </tbody>
     </table>
   );
