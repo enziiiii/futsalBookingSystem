61fd979d1b63a71001199b8f732df51a44f85e1b
enziiiii <enzii.grg00@gmail.com>
2025-04-18 01:06:37 +0545

    updated Error Handler

--- backend/server/middlewares/errorHandler.js
+++ backend/server/middlewares/errorHandler.js
@@ -1,6 +1,11 @@
 // Centralized error handling
 const errorHandling = (err, req, res , next) => {
     console.log(err.stack);
+    if (res.headerSent) {
+        console.warn('Headers already sent, skipping error response');
+        return next(err);
+    }
+
     res.status(500).json({
         status: 500,
         message: "Something went wrong",
