de9bc5ef5a1f18e1d24cfcf73dabcdaba4cf99c4
enziiiii <enzii.grg00@gmail.com>
2025-04-18 08:17:06 +0545

    updated db.js

--- backend/server/config/db.js
+++ backend/server/config/db.js
@@ -26,31 +26,6 @@ const startPool = () => {
         
 };
 
-// Initialize the pool once
-// pool = startPool();
-
-/* // Database connection configuration
-const pool = new Pool({
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	database: process.env.DB_NAME,
-});
-
-// Test connection
-// pool.on('connect', () => {
-//     console.log('Connected to PostgreSQL');
-// });
-  
-pool.on('error', (err) => {
-    console.error('Unexpected error on idle client', err);
-    process.exit(-1);
-});
-
-*/
-
-
 const connectDb = async () => {
     try {
         const client = await pool.connect(); // Get a client from the pool
