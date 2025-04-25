c8e037738b577c6efc9a3e647ce20cc5262c45ad
enziiiii <enzii.grg00@gmail.com>
2025-04-25 10:18:26 +0545

    updated password model

--- backend/server/models/password-model.js
+++ backend/server/models/password-model.js
@@ -1,13 +1,27 @@
-const { pool } = require("../config/db")
+const { pool } = require("../config/db");
+const { getUserById } = require("./user-model");
 class Password {
-    async updatePassword(userID, passwordHash) {
-        const query = `
-            UPDATE users
-            SET password_hash = $1
-            WHERE user_id = $2
-        `;
-        await pool.query(query, [passwordHash, userId]);
-    }
+    async updatePassword(userId, { oldPassword, newPassword }) {
+        const user = await getUserById(userId);
+        if (!user) throw new Error("User not found");
+
+        if (oldPassword) {
+            const isMatch = await bcrypt.compare(oldPassword, user.passwordHash);
+            if (!isMatch) throw new Error("Incorrect current password");
+        }
+
+        // hash the new password
+        const hashedNewPasssword = await bxrypt.hash(newPassword, 10);
+
+        // udpate the passwordHash field in the database
+        await pool.query(
+            "UPDATE users SET password_hash = $1 WHERE user_id = $2",
+            [hashedNewPasssword, userId]
+        );
+
+        return { messsage: "Password updated successfully" };
+    };
+
 
     async incrementTokenVersion(userId){
         const query =  `
