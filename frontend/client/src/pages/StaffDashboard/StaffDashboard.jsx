6ed9a12f46df0187fa74ab00a24b017a1456b5d9
enziiiii <enzii.grg00@gmail.com>
2025-04-29 11:50:19 +0545

    updated StaffDashboard.jsx

--- frontend/client/src/pages/StaffDashboard/StaffDashboard.jsx
+++ frontend/client/src/pages/StaffDashboard/StaffDashboard.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
+import Sidebar from '../../components/Sidebar';
+
 
 const StaffDashboard = () => {
+  const { user }  = useSelector((state) => state.auth);
+  if (!user || !user.roles.includes('staff')) {
+    return <Navigate to ="/login" />;
+  }
+
   return (
-    <div>StaffDashboard</div>
+    <div className="flex h-screen bg-gray-100">
+      <Sidebar role="staff" />
+      <div className="flex-1 p-6 ml-16 md-ml-64">
+        <Outlet />
+        {/* <h1>Hello from StaffDashboard</h1> */}
+      </div>
+    </div>
   )
 }
 
