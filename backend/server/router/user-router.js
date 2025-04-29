5fda3fda5dd97def5ef5df8421831c6c5e3c9a0e
enziiiii <enzii.grg00@gmail.com>
2025-04-29 11:36:24 +0545

    updated user-router.js

--- backend/server/router/user-router.js
+++ backend/server/router/user-router.js
@@ -5,10 +5,19 @@ const { validateUser } = require("../middlewares/inputValidator");
 
 const router = express.Router();
 
+// User Registration Route with zod input validator
 router.post("/", validateUser('registerSchema'), userController.createUserController);
+
+// To get all User Route
 router.get("/", userController.getAllUsersController);
+
+// To get User by Id
 router.get("/:userId", userController.getUserByIdController);
+
+// To update User by Id
 router.put("/:userId", userController.updateUserController);
+
+// To Delete User by Id
 router.delete("/:userId", userController.deleteUserController);
 
 module.exports = router;
