3d38a7b0ad57787ac790f2121d16c3c7104f7612
enziiiii <enzii.grg00@gmail.com>
2025-04-29 11:34:13 +0545

    updated admin router

--- backend/server/router/admin-router.js
+++ backend/server/router/admin-router.js
@@ -6,32 +6,68 @@ const { validateCourt, validateUser } = require("../middlewares/inputValidator")
 const courtController = require("../controllers/court-controller");
 const userController = require("../controllers/user-controller");
 const roleController = require("../controllers/role-controller");
+const bookingController = require("../controllers/booking-controller");
+const analyticsController = require("../controllers/analytics-controller");
 const router = express.Router();
 
 
 
-// Admin routes
+// -- Admin routes
 // router.route("/admin-UserController").get(userController.getAllUsersController);
 router.get("/admin-dashboard", protect, authorize(['admin']), adminController.getAdminDashboard);
 
-
-// Court management routes
+// To create Court by Admin with zod input validaotr
 router.post("/courts", protect, authorize(["admin"]), validateCourt('createCourtSchema'), courtController.createCourtController);
-router.get("/courts", protect, authorize(["admin"]), courtController.getAllCourtsController);
-// router.get("/courts", courtController.getAllCourtsController);
 
+// To get all courts as Admin
+router.get("/courts", protect, authorize(["admin"]), courtController.getAllCourtsController);
+
+// To court by ID as Admin
 router.get("/courts/:courtId", protect, authorize(["admin"]), courtController.getCourtByIdController);
+
+// To update the court as Admin with zod input validator
 router.put("/courts/:courtId", protect, authorize(["admin"]), validateCourt('updateCourtSchema'), courtController.updateCourtController);
+
+// To delete court As Admin
 router.delete("/courts/:courtId", protect, authorize(["admin"]), courtController.deleteCourtController);
 
-// Admin-customer management routes
+// -- Admin-customer management routes
 router.get("/users", protect, authorize(["admin"]), userController.getAllUsersController);
+
 // router.get("/users", protect, authorize(["admin"]), roleController.getUsersByRoleController);
+
+// To create User by Admin with zod input validator
 router.post("/users", protect, authorize(["admin"]), validateUser('createUserSchema'), userController.createUserController);
+
+// To update User by Admin with zod input validator
 router.put("/users/:userId", protect, authorize(["admin"]), userController.updateUserController);
+
+// To delete User by Admin with zod input validator
 router.delete('/users/:userId', protect, authorize(['admin']), userController.deleteUserController);
 
-// Admin-staff management routes
+// To get all bookings made by customers
+router.get("/bookings", protect, authorize(["admin"]), bookingController.getAllBookingsController);
+
+// --------- Admin-staff management routes
+
+
+
+
+
+// -------- admin overseeing reports/analytics
+// booking reports
+router.get("/analytics/bookings", protect, authorize(["admin"]), analyticsController.getBookingAnalyticsController);
+
+// to get all revenue
+router.get("/analytics/revenue", protect, authorize(["admin"]), analyticsController.getRevenueAnalyticsController);
+
+// for admin to get all court utilization analytics
+router.get("/analytics/court-utilization", protect, authorize(["admin"]), analyticsController.getCourtUtilizationController);
+
+
+
+// Admin-role management routes
+router.put('/users/:userId/roles', protect, authorize(['admin']), userController.updateUserRoles)
 
 // Admin getting role
 router.get("/admin/users", protect, authorize(["admin"]), (req, res, next) => {
