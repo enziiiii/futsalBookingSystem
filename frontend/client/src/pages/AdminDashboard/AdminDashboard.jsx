42f1f19a64e8b8ce0404b24219845f8cd038be46
enziiiii <enzii.grg00@gmail.com>
2025-04-29 11:43:55 +0545

    updated AdminDashboard.jsx

--- frontend/client/src/pages/AdminDashboard/AdminDashboard.jsx
+++ frontend/client/src/pages/AdminDashboard/AdminDashboard.jsx
@@ -1,28 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
+import Sidebar from '../../components/Sidebar';
 
 
 const AdminDashboard = () => {
-  console.log('UserNav rendered')
   
   return (
     <div>
       <div className="flex h-screen">
-      <aside className="w-64 bg-gray-800 text-white p-4">
-        <nav>          
-          <ul>
-            {/* <li><Link to="/admin/profile" className="block py-2">Profile</Link></li> */}
-            <li><Link to="/admin/courts" className="block py-2">Manage courts</Link></li>
-            <li><Link to="/admin/manageCustomers" className="block py-2">Manage Customers</Link></li>
-            <li><Link to="/admin/manageStaffs" className="block py-2">Manage Staffs</Link></li>
-            <li><Link to="/admin/logOut" className="block py-2">Log Out</Link></li>
-          </ul>
-        </nav>
-      </aside>
-      <main className="flex-1 p-4">
-        <h2 className="text-3xl font-bold mb-4">Admin Dashboard</h2>
-          <p> Welcome to Admin! Below are courts and user manangement</p>
-      </main>
+        <Sidebar role="admin" />
+        <div className="flex-1 p-6 ml-16 md:ml-64">
+          <Outlet />
+        </div>
     </div>
   </div>
     
