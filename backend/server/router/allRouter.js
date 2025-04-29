8410fcd9d36636ada8cd17471ce5178037c404fe
enziiiii <enzii.grg00@gmail.com>
2025-04-29 11:34:34 +0545

    updated allRouter.js

--- backend/server/router/allRouter.js
+++ backend/server/router/allRouter.js
@@ -6,13 +6,14 @@ const router = express.Router();
 const authRouter = require("./auth-router.js");
 const userRouter = require("./user-router");
 const adminRouter = require("./admin-router.js");
+const staffRouter = require("./staff-router.js");
 const customerRouter = require("./customer-router.js");
 
 // Use sub-routers
 router.use("/auth", authRouter);
 router.use("/users", userRouter);
 router.use("/admin", adminRouter);
+router.use("/staff", staffRouter);
 router.use("/customer", customerRouter);
-
 
 module.exports = router;
