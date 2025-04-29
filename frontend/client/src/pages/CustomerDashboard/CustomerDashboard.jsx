0ecb22e4b8bba8e84d8b478b2833a0014111dad3
enziiiii <enzii.grg00@gmail.com>
2025-04-29 11:46:19 +0545

    upated CustomerDashboard.jsx

--- frontend/client/src/pages/CustomerDashboard/CustomerDashboard.jsx
+++ frontend/client/src/pages/CustomerDashboard/CustomerDashboard.jsx
@@ -1,10 +1,18 @@
 import React, { useEffect } from 'react'
 
 import { useDispatch, useSelector } from 'react-redux'
-import Court from './Court';
 import { fetchCourtsForCustomer } from '../../reducers/courtsSlice';
+import Sidebar from '../../components/Sidebar';
+import { Navigate, Outlet } from 'react-router-dom';
+
+
 
 const CustomerDashboard = () => {
+  const { user } = useSelector((state) => state.auth);
+  if (!user || !user.roles.includes('customer')) {
+    return <Navigate to="/login" />;
+  }
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -14,11 +22,13 @@ const CustomerDashboard = () => {
 
 
   return (
-    <div>
-      <h1>Customer Dashboard</h1>
-      <Court />
-
+    <div className="flex">
+      <Sidebar role="customer" />
+      <div className="flex-1 p-6 ml-16 md:ml-64">
+        <Outlet />
+      </div>
     </div>
+
   );
 
 };
