32e241458096fc48b08167ff73c313b6a02f5f38
enziiiii <enzii.grg00@gmail.com>
2025-04-29 11:24:42 +0545

    updated role model.js

--- backend/server/models/role-model.js
+++ backend/server/models/role-model.js
@@ -6,21 +6,6 @@ const getRoleByName = async (roleName) => {
   return role;
 };
 
-
-// const getUsersByRole = async (roleName) => {
-//   const query = `
-//     SELECT u.*, COALESCE(array_agg(r.role_name) FILTER (WHERE r.role_name IS NOT NULL), '{}') as roles
-//     FROM users u
-//     JOIN user_roles ur ON u.user_id = ur.user_id
-//     JOIN roles r ON ur.role_id = r.role_id
-//     WHERE r.role_name = $1
-//     GROUP BY u.user_id
-//   `;
-//   const result = await pool.query(query, [roleName]);
-//   return result.rows;
-// };
-
-
 module.exports = {
   getRoleByName
 
